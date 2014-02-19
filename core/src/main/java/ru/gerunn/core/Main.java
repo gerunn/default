461701137fa810c840196280cab09d657c7c9bc7
gerunn <gerunn@gmail.com>
2014-02-20 00:48:43 +0400

    .

---
+++ core/src/main/java/ru/gerunn/core/Main.java
@@ -0,0 +1,12 @@
+package ru.gerunn.core;
+
+/**
+ * Created by human on 2/20/14.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Helloe!1");
+    }
+
+}
---
+++ core/src/test/java/ru/gerunn/core/UTestMain.java
@@ -0,0 +1,15 @@
+package ru.gerunn.core;
+
+import org.junit.Test;
+
+/**
+ * Created by human on 2/20/14.
+ */
+public class UTestMain {
+
+    @Test
+    public void uTestMain() {
+        System.out.println("Running UTestMain.uTestMain...");
+    }
+
+}
